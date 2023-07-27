b2c3c0644dc353f892a90a4ca7e001bf7d32c908
WANDEXX <55203271+WJMD@users.noreply.github.com>
2023-07-27 15:58:14 -0400

    creacion de Controlador,repositorio y servicio

---
+++ 1-CRUD-SpringBoot/src/main/java/com/app/web/controlador/EstudianteControlador.java
@@ -0,0 +1,20 @@
+package com.app.web.controlador;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.app.web.repositorio.EstudianteRepositorio;
+
+@Controller
+public class EstudianteControlador {
+
+	@Autowired
+	private EstudianteRepositorio repositorio;
+	
+	@GetMapping({"/estudiantes","/"})
+	public String  listarEstudiantes() {
+		return "estudiantes";//Nos retorna al achivo estudiantes
+	}
+	
+}
---
+++ 1-CRUD-SpringBoot/src/main/java/com/app/web/servicio/listarTodosLosEstudiantesImpl.java
@@ -0,0 +1,22 @@
+package com.app.web.servicio;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.app.web.entidad.Estudiante;
+import com.app.web.repositorio.EstudianteRepositorio;
+
+@Service
+public class listarTodosLosEstudiantesImpl implements EstudianteServicio{
+
+	@Autowired
+	private EstudianteRepositorio repositorio;
+	
+	@Override
+	public List<Estudiante> listarTodosLosEstudiantes() {
+		return repositorio.findAll();
+	}
+ 
+}
