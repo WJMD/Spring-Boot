ca13c429504f6e05443951cd9fbe1e9e8f9346d0
WANDEXX <55203271+WJMD@users.noreply.github.com>
2023-07-31 11:38:10 -0400

    creacion de repositorio, servicio, controller

---
+++ 2-Agenda-Contactos-SpringBoot/src/main/java/com/agenda/contactos/servicio/contactoSercivio.java
@@ -0,0 +1,8 @@
+package com.agenda.contactos.servicio;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class contactoSercivio {
+
+}
--- 2-Agenda-Contactos-SpringBoot/src/test/java/com/agenda/contactos/ApplicationTests.java
+++ 2-Agenda-Contactos-SpringBoot/src/test/java/com/agenda/contactos/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.agenda.contatos;
+package com.agenda.contactos;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
